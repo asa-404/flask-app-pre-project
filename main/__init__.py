84b8d5ffbb7a7701f51535462dd2171a9f2d839f
Karthiksrinatha <94411439+Karthiksrinatha@users.noreply.github.com>
2022-02-13 15:36:34 -0800

    Environment Variable

--- main/__init__.py
+++ main/__init__.py
@@ -7,7 +7,7 @@ basedir = os.getcwd()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("POSTGRES_DB_AWS")
 
 
 db = SQLAlchemy(app)
